430fb5a42281dcf93e217f0593d30bcda0c7f4c2
Carmine Toriello <carminetoriello95@gmail.com>
2020-09-21 17:58:02 +0200

    Models created

---
+++ lib/model/cart_item.dart
@@ -0,0 +1,9 @@
+import 'product.dart';
+
+class CartItem {
+  int id;
+  Product product;
+  int quantity;
+
+  CartItem({this.id,this.product, this.quantity});
+}
---
+++ lib/model/product.dart
@@ -0,0 +1,16 @@
+class Product {
+  final int id;
+  final String imagePath;
+  final double price;
+  final String name;
+  final String weight;
+  final String description;
+
+  const Product(
+      {this.id,
+      this.imagePath,
+      this.price,
+      this.name,
+      this.weight,
+      this.description});
+}
